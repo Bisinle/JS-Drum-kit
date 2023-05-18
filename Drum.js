e34bec62f2477d3dac19f768f06ced9456c55a97
bisinle <abdiwadud.mohamedd@gmail.comm>
2023-05-18 18:50:10 +0300

    sound added

--- Drum.js
+++ Drum.js
@@ -19,3 +19,48 @@ audio_A.setAttribute('src','./sounds/clap.wav')
 document.body.append(audio_A)
 
 
+const  hithatDiv = document.createElement('div')
+const S_key = document.createElement('kbd')
+const S_span = document.createElement('span')
+hithatDiv.classList.add('key')
+hithatDiv.setAttribute('data-key','83')
+S_key.textContent='S'
+hithatDiv.append(S_key)
+S_span.textContent = 'hihat'
+hithatDiv.append(S_span)
+mainDiv.append(hithatDiv)
+const audio_S = document.createElement('audio')
+audio_S.setAttribute('data-key','83')
+audio_S.setAttribute('src','./sounds/hihat.wav')
+document.body.append(audio_S)
+
+
+const boomDiv = document.createElement('div')
+const D_key = document.createElement('kbd')
+const D_span = document.createElement('span')
+boomDiv.classList.add('key')
+boomDiv.setAttribute('data-key', '68')
+D_key.textContent = 'D'
+boomDiv.append(D_key)
+D_span.textContent = 'Boom'
+boomDiv.append(D_span)
+mainDiv.append(boomDiv)
+
+
+
+
+
+
+
+
+
+
+
+
+window.addEventListener('keydown', function(event){
+  const audio =document.querySelector(`audio[data-key="${event.keyCode}"]`)
+  if(!audio) return;
+  audio.currentTime =0;
+  audio.play();
+
+})
